effa0b2c518217cb32d7abe32b170c6868f943a7
MirsonCamadro <kztempus@gmail.com>
2020-06-29 18:40:57 -0400

    added routes to App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './App.css';
+import HomeContainer from './containers/HomeContainer';
+import NewsContainer from './containers/NewsContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter } from 'react-router-dom'
+import TwitchContainer from './containers/TwitchContainer';
 
-const App = () => {
-    return ( <div>wena ctm</div>)
+class App extends Component {
+
+  renderHome = () => <HomeContainer/>;
+  renderNews = () => <NewsContainer/>;
+  renderTwitch = () => <TwitchContainer/>;
+
+  render(){
+    return (
+        <BrowserRouter>
+            <div>
+            <Route exact path="/" component={this.renderHome}/>
+            <Route exact path="/twitch" component={this.renderTwitch}/>
+            <Route exact path="/news" component={this.renderNews}/>
+                <Switch>
+                    <Route path="/news/:slug" component={NewCustomerContainer} />
+  	  	        </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
