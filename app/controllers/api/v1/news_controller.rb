294d359b6b56c3232ea017ae40ef474e251d7597
MirsonCamadro <kztempus@gmail.com>
2020-06-29 18:21:21 -0400

    added protect_from_forgery to put and patch request

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,6 +1,8 @@
 module Api
     module V1
         class NewsController < ApplicationController
+            protect_from_forgery with: :null_session
+
             def index
                 news = News.all
                 
