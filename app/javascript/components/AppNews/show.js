b11cc2e1a86c82fdd9724aa2d3b9aad01a82978a
MirsonCamadro <kztempus@gmail.com>
2020-07-01 13:13:59 -0400

    added ranking_avg to news serializer and twitch user render ok

--- app/javascript/components/AppNews/show.js
+++ app/javascript/components/AppNews/show.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Show = () => {
+const Show = (props) => {
+    const [airline, setAirline] = useState({})
+    const [review, setReviews] = useState({})
+
+    useEffect(() => {
+        const slug = props.match.params.slug
+        const url = `/api/v1/news/${slug}`
+        console.log(props)
+    }, [])
+
     return <div>este es el show</div>
 }
 
--- app/javascript/components/AppTwitchUsersList/AppTwitchUser.js
+++ app/javascript/components/AppTwitchUsersList/AppTwitchUser.js
@@ -1,26 +1,60 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {Row, Col} from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import {Row, Col} from 'react-bootstrap';
 
-class AppTwitchUser extends Component {
-    render() {
-        return (
-            <div>
-                <Row className=" row-image-twitch mb-4 mt-2">
-                    <Col sm={{span:6, offset:1}}>
-                        <a href="www.google.cl"> 
-                            <img className="image-clans-home " src='./images/assets/TW-Vistima.png' alt="vistima"/> 
-                            <span className="name-clans-home">Vistima</span>
-                        </a>
-                    </Col>
-                    <Col sm={4} className="my-auto">
-                        <span className="dot"></span> 100
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
-}
+// class AppTwitchUser extends Component {
+//     render() {
+//         return (
+            // <div>
+            //     <Row className=" row-image-twitch mb-4 mt-2">
+            //         <Col sm={{span:6, offset:1}}>
+            //             <a href="www.google.cl"> 
+            //                 <img className="image-clans-home " src='./images/assets/TW-Vistima.png' alt="vistima"/> 
+            //                 <span className="name-clans-home">Vistima</span>
+            //             </a>
+            //         </Col>
+            //         <Col sm={4} className="my-auto">
+            //             <span className="dot"></span> 100
+            //         </Col>
+            //     </Row>
+            // </div>
+//         );
+//     }
+// }
+
+// AppTwitchUser.propTypes = {
+
+// };
+
+// export default AppTwitchUser;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Row, Col } from 'react-bootstrap';
+import './../../../assets/stylesheets/home.css';
+
+
+const AppTwitchUser = ({ twitches }) => {
+    return (
+        <div className="container-fluid">
+                { twitches.map( twitch =>  
+                    <div key={twitch.id}>
+                        <Row className=" row-image-twitch mb-4 mt-2">
+                            <Col sm={{span:7}}>
+                                <a href="www.google.cl"> 
+                                    <img className="image-clans-home " src={twitch.attributes.twitch_img} alt={twitch.attributes.slug}/> 
+                                    <span className="name-clans-home">{twitch.attributes.streamer}</span>
+                                </a>
+                            </Col>
+                            <Col sm={4} className="my-auto">
+                                <span className="dot"></span> 100
+                            </Col>
+                        </Row>
+                    </div> 
+                )}
+        </div>
+    )
+};
 
 AppTwitchUser.propTypes = {
 
