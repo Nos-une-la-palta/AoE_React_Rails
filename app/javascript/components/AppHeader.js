0e3c3a4fc96a562c110f06c64b0a15e7d017e22c
PachecoCesar <c86pacheco@gmail.com>
2020-07-14 12:40:57 -0400

    style changes in jumbotron, adding claim and dropdown button

--- app/javascript/components/AppHeader.js
+++ app/javascript/components/AppHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './../../assets/stylesheets/home.css'
-import { Container, Row, Col, Button} from 'react-bootstrap';
+import { Container, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap';
 
 class AppHeader extends Component {
     render() {
@@ -10,7 +10,20 @@ class AppHeader extends Component {
                 <Container fluid>
                     <Row>
                         <Col sm={{span: 1, offset: 11,}} className="mt-3">
-                             <Button  variant="warning">Log in</Button>
+                            <DropdownButton
+                              variant="warning"
+                              title="Acceso"
+                              id="input-group-dropdown-2"
+                            >
+                              <Dropdown.Item href="#">Ingreso</Dropdown.Item>
+                              <Dropdown.Divider />
+                              <Dropdown.Item href="#">Registro</Dropdown.Item>
+                            </DropdownButton>
+                        </Col>
+                    </Row>
+                    <Row className="mt-2">
+                        <Col sm={{span: 3, offset: 8}} className="mt-5">
+                            <span className="header-claim"><b>Chile AOE</b> es una comunidad formada por jugadores de Age of Empire con más de 15 años de experiencia, siendo uno de los más antiguos en Chile.</span>
                         </Col>
                     </Row>
                 </Container>
