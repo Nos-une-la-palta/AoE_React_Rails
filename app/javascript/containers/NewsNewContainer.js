740dd6967a4af69bfa3a8490aa40338f713b7230
MirsonCamadro <kztempus@gmail.com>
2020-07-20 12:25:14 -0400

    not working yet

--- app/javascript/containers/NewsNewContainer.js
+++ app/javascript/containers/NewsNewContainer.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 import {Container, Row, Col } from "react-bootstrap";
 import AppFrame from '../components/AppFrame';
 import AppNewsNew from "../components/AppNews/AppNewsNew"
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { insertNews } from './../actions/insertNews'
 
 class NewsNewContainer extends Component {
+
+    handleSubmit = () => {
+
+    }
+
+    handleOnSubmitSuccess = () => {
+        
+    }
+
+    handleOnBack = () => {
+        this.props.history.goBack()
+    }
+
+
+    renderBody = () => {
+        return (
+            <AppNewsNew
+                onSubmit={this.hadleSubmit}
+                onSubmitSuccess={this.handleOnSubmitSuccess}
+                onBack={this.handleOnBack}
+                />
+        )
+    }
+
     render() {
         return(
             <div>
                 <AppFrame body={
-                <Container fluid>
-                    <Row className="mt-5">
-                        <Col xs={{span: 6, offset: 3,}}>
-                            <AppNewsNew />
-                        </Col>
-                    </Row>
-                </Container>
+                    <Container fluid>
+                        <Row className="mt-5">
+                            <Col xs={{span: 6, offset: 3,}}>
+                                {this.renderBody()}
+                            </Col>
+                        </Row>
+                    </Container>
                 }/>
             </div>
         )
@@ -22,4 +49,4 @@ class NewsNewContainer extends Component {
 }
 
 
-export default NewsNewContainer;
+export default withRouter(connect(null, { insertNews })(NewsNewContainer));
